92c763d5098171773535c25c4618d6623906df24
Jeswin-J <jeswinjoseph164@gmail.com>
2024-07-31 20:07:40 +0530

    Exercise 7: Financial Forecasting

---
+++ Week_1_Exercises/FinancialForecasting/src/FinancialForecast.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class FinancialForecast {
+    public static double calculateFutureValue(double initialValue, double growthRate, int period){
+        HashMap<Integer, Double> map = new HashMap<>();
+
+        if(period == 0){
+            map.put(period, initialValue);
+        } else {
+            double result = calculateFutureValue(initialValue * (1 + growthRate), growthRate, period - 1);
+            map.put(period, result);
+        }
+        return map.get(period);
+    }
+}

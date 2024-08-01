23d8220d0dfd6e98c5456044fcdb962639308aae
Jeswin-J <jeswinjoseph164@gmail.com>
2024-08-01 11:29:53 +0530

    Exercise 1: Implementing the Singleton Pattern

---
+++ Week_1_Exercises/Design Patterns/SingletonPatternExample/src/Logger.java
@@ -0,0 +1,22 @@
+public class Logger {
+    private static Logger loggerInstance;
+    private static final String message = "Logging Success!";
+
+    private Logger(){
+    }
+
+    public static Logger getLoggerInstance() {
+        if (loggerInstance == null) {
+            loggerInstance = new Logger();
+        }
+        return loggerInstance;
+    }
+
+    public static void setLoggerInstance(Logger loggerInstance) {
+        Logger.loggerInstance = loggerInstance;
+    }
+
+    public void logMessage(){
+        System.out.println("LOG: " + Logger.message);
+    }
+}
